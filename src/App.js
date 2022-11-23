4ca690b526edf41209604513ba3e5b9e209d2b2c
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2022-11-23 20:24:37 +0900

    tailwindcssのheaderテンプレートを設置

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import Header from './components/Header';
 import './App.css';
 
 const App = () => {
   return (
-    <div className='text-3xl font-bold underline'>Instagram</div>
+    <Header />
   );
 }
 
